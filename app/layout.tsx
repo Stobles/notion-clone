afb9ddd33ee2d1aa1a652b91ec7e8ee84c9b2735
ching <ilovespnjensen@inbox.ru>
2023-11-18 17:00:54 +0300

    init

---
+++ app/layout.tsx
@@ -0,0 +1,6 @@
+import { App } from '@/app';
+import '@/app/globals.css';
+
+export default App;
+
+
---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/shared/UI/Button";
+
+export function HomePage() {
+  return (  
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
